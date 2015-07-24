d94892ff1a873d03b2af95df77ecaa75c8cbe770
Demotive <mat@demotive.com>
2015-07-24 14:08:21 +0100

    First lump of V3 code

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,81 @@ module.exports = {
 
     // add your routes here
 
+    // V3 --------------------------------------------------------
+
+    app.get('/v3/conveyancer/*', function(req, res, next) {
+
+      var n = req.session.views || 0;
+      req.session.views = ++n;
+      console.log('views: ' + req.session.views);
+
+      console.log('new_case: ' + req.session.new_case);
+      console.log('case_reference: ' + req.session.case_reference);
+
+      //console.log('displayProperty: ' + req.session.displayProperty);
+      //console.log('displayBorrower_1: ' + req.session.displayBorrower_1);
+      //console.log('displayBorrower_2: ' + req.session.displayBorrower_2);
+      console.log('\n');
+
+      next();
+    });
+
+    // To help. A list of all the session vars used:
+
+    // req.session.new_case - true / false - controls display of "new" case in case list
+    // req.session.case_reference - string / undefined - customisable reference number
+
+
+    // PAGE requests --------------------
+
+    // Sign in page ALWAYS flushes the session
+    app.get('/v3/conveyancer/login', function (req, res) {
+      // destroy the session:
+      req.session = null;
+      res.render('v3/conveyancer/login');
+    });
+
+    // Case List - send this page some session vars:
+    app.get('/v3/conveyancer/case-list', function (req, res) {
+      res.render('v3/conveyancer/case-list', {
+        "new_case": req.session.new_case,
+        "case_reference": req.session.case_reference
+      });
+    });
+
+    // Case - send this page some session vars:
+    app.get('/v3/conveyancer/case', function (req, res) {
+      req.session.new_case = true;
+      res.render('v3/conveyancer/case', {
+        "case_reference": req.session.case_reference
+      });
+    });
+
+    // Case Reference - send this page a session var:
+    app.get('/v3/conveyancer/case-reference', function (req, res) {
+      res.render('v3/conveyancer/case-reference', {
+        "case_reference": req.session.case_reference
+      });
+    });
+
+    // Case Find Property - send this page a session var:
+    app.get('/v3/conveyancer/case-find-property', function (req, res) {
+      res.render('v3/conveyancer/case-find-property', {
+        "case_reference": req.session.case_reference
+      });
+    });
+
+    // HANDLERS --------------------
+
+    // Reference handler
+    app.get('/v3/conveyancer/case-reference-handler', function (req, res) {
+      req.session.case_reference = req.query.caseref;
+      res.redirect('/v3/conveyancer/case');
+    });
+
+    // ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
+
     // v2 step 1 - create case
 
     app.get('/v2/step1/*', function(req, res, next) {
