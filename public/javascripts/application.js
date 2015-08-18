6956dec462c732056d04680951037284d38707d2
Andy Porter <andyporter@AndyPorter-MacBook.local>
2015-08-18 19:12:31 +0100

    logo to root of 3.1 (volatile) or 3 (static) + hardcode completed registration

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,13 @@
 function gotoRoot() {
-  window.location.href = '/v3/';
+  if (window.location.href.indexOf("/v3-1/") > 1) {
+  	window.location.href = '/v3-1/';
+  }
+  else {
+  	window.location.href = '/v3/';
+  }
   return false;
 }
+
 
 var logo = document.getElementById('logo');
 logo.onclick = gotoRoot;
